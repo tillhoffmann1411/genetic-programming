502fe07084a15a2b8b7bcb52fc3bab5e9b0a1586
Till Hoffmann <tillhoffmann1411@gmail.com>
2021-04-23 15:32:20 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Using Genetic Programming for Portfolio generation
+
+## References
+### Packages
+Deap Package: https://deap.readthedocs.io/en/master/
+numpy: https://numpy.org/
+
+### Data
+Data from: https://www.kaggle.com/dsv/1054465
---
+++ helper.py
@@ -0,0 +1,40 @@
+from deap.tools.support import Logbook
+import matplotlib.pyplot as plt
+import networkx as nx
+import pygraphviz as pgv
+
+
+def plot(log: Logbook):
+  gen = log.select("gen")
+  fit_mins = log.chapters["fitness"].select("min")
+  size_avgs = log.chapters["size"].select("avg")
+
+  fig, ax1 = plt.subplots()
+  line1 = ax1.plot(gen, fit_mins, "b-", label="Minimum Fitness")
+  ax1.set_xlabel("Generation")
+  ax1.set_ylabel("Fitness", color="b")
+  for tl in ax1.get_yticklabels():
+      tl.set_color("b")
+
+  ax2 = ax1.twinx()
+  line2 = ax2.plot(gen, size_avgs, "r-", label="Average Size")
+  ax2.set_ylabel("Size", color="r")
+  for tl in ax2.get_yticklabels():
+      tl.set_color("r")
+
+  lns = line1 + line2
+  labs = [l.get_label() for l in lns]
+  ax1.legend(lns, labs, loc="center right")
+  plt.show()
+
+def plot_tree(nodes, edges, labels):
+  g = pgv.AGraph()
+  g.add_nodes_from(nodes)
+  g.add_edges_from(edges)
+  g.layout(prog="dot")
+
+  for i in nodes:
+      n = g.get_node(i)
+      n.attr["label"] = labels[i]
+
+  g.draw("tree.png")
