f096ad14084910ed1d3c158c6e5f3211507b93ea
Till Hoffmann <tillhoffmann1411@gmail.com>
2021-04-30 21:33:01 +0200

    added gp math example

--- gen_code.py
+++ gen_code.py
@@ -2,3 +2,35 @@
 round(add(sub(mul(mul(add(3, 8), add(8, 4)), 6), 3), mul(mul(protectedDiv(sub(sub(7, 0), 0), protectedDiv(7, 1)), 5), 9)))
 # GP generated code
 round(mul(mul(1, 9), mul(8, 6)))
+# GP generated code
+round(mul(5, 9))
+# GP generated code
+round(add(mul(add(mul(add(mul(sub(8, 1), sub(add(9, 9), 3)), sub(8, 1)), protectedDiv(mul(8, 3), protectedDiv(5, 8))), sub(8, 1)), sub(2, 4)), mul(mul(mul(mul(8, 8), 8), protectedDiv(add(mul(8, sub(add(9, 9), 3)), sub(8, 1)), protectedDiv(8, 4))), protectedDiv(8, 1))))
+# GP generated code
+round(add(mul(add(add(add(mul(protectedDiv(8, 3), mul(sub(9, 5), add(8, 8))), mul(mul(mul(8, 1), add(2, 4)), 7)), mul(mul(sub(9, 5), add(8, 8)), mul(protectedDiv(8, 3), mul(sub(9, 5), add(8, 8))))), add(7, 0)), mul(protectedDiv(4, 2), add(2, 9))), mul(mul(protectedDiv(1, 4), protectedDiv(8, 8)), sub(mul(0, 6), 8))))
+# GP generated code
+round(mul(mul(mul(mul(7, 9), 9), sub(mul(7, 9), add(8, 6))), 9))
+# GP generated code
+round(add(mul(mul(9, 4), mul(8, 4)), mul(mul(2, 7), mul(mul(9, 3), 4))))
+# GP generated code
+round(mul(add(add(7, 3), mul(9, 3)), mul(9, 8)))
+# GP generated code
+round(mul(add(mul(9, 6), 4), mul(mul(3, 3), 5)))
+# GP generated code
+round(mul(mul(sub(mul(7, 8), protectedDiv(2, 7)), 8), 6))
+# GP generated code
+round(mul(add(8, 6), mul(4, mul(6, 8))))
+# GP generated code
+round(mul(mul(6, add(add(4, 3), add(5, 4))), mul(7, 4)))
+# GP generated code
+round(add(mul(mul(mul(4, 7), 2), mul(add(2, 9), 2)), mul(mul(7, 8), mul(7, 3))))
+# GP generated code
+round(mul(add(mul(sub(6, 0), mul(6, 7)), mul(mul(5, 5), 1)), sub(9, 0)))
+# GP generated code
+round(mul(add(sub(add(4.0, 4.0), protectedDiv(4.0, 4.0)), mul(add(4.0, 4.0), 4.0)), mul(mul(4.0, 4.0), mul(4.0, protectedDiv(4.0, 4.0)))))
+# GP generated code
+sub(mul(x, x), sub(sub(x, 4.0), x))
+# GP generated code
+sub(mul(add(sub(sub(1, 8), mul(6, 9)), mul(x, 5)), add(add(x, 0), mul(0, 3))), mul(6, mul(add(add(x, 4), add(4, 2)), protectedDiv(x, 5))))
+# GP generated code
+mul(x, add(add(sub(sub(5, 0), protectedDiv(9, x)), mul(2, x)), x))
